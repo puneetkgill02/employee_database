7636f8970949f575a5e08b3cf133dc7c4b6967d1
Puneet Gill <115737939+puneetkgill02@users.noreply.github.com>
2023-02-14 09:43:20 -0800

    Add files via upload
    SQL files are attached as well as a README to explain the process in finding specific information within the different csv files.

---
+++ employee_database.sql
@@ -0,0 +1,36 @@
+create table salaries(
+	emp_no int,
+	salary int
+)
+
+create table dept_manager(
+	dept_no varchar,
+	emp_no int
+)
+
+create table titles(
+	title_id varchar,
+	title varchar
+)
+
+create table dept_emp(
+	emp_no int,
+	dept_no varchar
+)
+
+create table departments(
+	dept_no varchar,
+	dept_name varchar
+)
+
+create table employees(
+	emp_no int,
+	emp_title_id varchar,
+	birth_date varchar,
+	first_name varchar,
+	last_name varchar,
+	sex varchar,
+	hire_date varchar
+)
+
+
---
+++ employee_database2.sql
@@ -0,0 +1,61 @@
+-- List the employee number, last name, first name, sex, and salary of each employee
+select e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+from employees as e
+join salaries as s
+on e.emp_no = s.emp_no
+
+--List the first name, last name, and hire date for the employees who were hired in 1986
+select first_name, last_name, hire_date
+from employees 
+where split_part(hire_date, '/', 3) = '1986'
+
+/*List the manager of each department along with their department number,
+department name, employee number, last name, and first name*/
+select d.dept_no, d.dept_name, dm.emp_no, e.last_name, e.first_name
+from departments as d
+join dept_manager as dm
+on dm.dept_no = d.dept_no
+	join employees as e
+	on e.emp_no = dm.emp_no
+
+/*List the department number for each employee along with that employee’s 
+employee number, last name, first name, and department name.*/
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from departments as d
+join dept_emp as de
+on de.dept_no = d.dept_no
+	join employees as e
+	on e.emp_no = de.emp_no
+	
+/*List first name, last name, and sex of each employee whose first name is 
+Hercules and whose last name begins with the letter B*/
+select first_name, last_name, sex
+from employees
+where first_name = 'Hercules' and last_name like 'B%'
+
+/*List each employee in the Sales department, including their 
+employee number, last name, and first name*/
+select e.emp_no, e.last_name, e.first_name
+from departments as d
+join dept_emp as de
+on de.dept_no = d.dept_no
+	join employees as e
+	on e.emp_no = de.emp_no
+where d.dept_name = 'Sales'
+
+/*List each employee in the Sales and Development departments, including their 
+employee number, last name, first name, and department name*/
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from departments as d
+join dept_emp as de
+on de.dept_no = d.dept_no
+	join employees as e
+	on e.emp_no = de.emp_no
+where d.dept_name in ('Sales', 'Development')
+
+/*List the frequency counts, in descending order, of all the employee last 
+names (that is, how many employees share each last name)*/
+select last_name, count(last_name) as "last name count"
+from employees 
+group by last_name
+order by "last name count" desc
